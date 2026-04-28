fe26a7c345b2c09a63c8f2499fc5792ff5f8a1a7
Vishnu Aji <vishnuaji2002@gmail.com>
2026-04-28 22:35:42 +0530

    Create Student.cs

---
+++ linq/Models/Student.cs
@@ -0,0 +1,11 @@
+namespace Linq.Models
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Grade { get; set; }
+        public string Department { get; set; }
+    }
+}
